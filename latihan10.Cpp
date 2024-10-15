6865e3db98e6b747c66a6f2bb93f4e42518d77d5
agusssjp <khususbelajarskd@gmail.com>
2024-10-14 20:38:11 -0700

    Add files via upload

---
+++ latihan10.Cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 10;
+  int y = 20;
+  cout << "Before swap: x = " << x << ", y = " << y << endl;
+
+  // Swap x and y
+  int temp = x;
+  x = y;
+  y = temp;
+
+  cout << "After swap: x = " << x << ", y = " << y << endl;
+  return 0;
+}
---
+++ latihan6.Cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 10, y = 20;
+  int z = x + y;
+  cout << "Sum of x and y is " << z << endl; //kurang titik koma ;
+  return 0;
+}
